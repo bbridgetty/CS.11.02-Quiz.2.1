90cfec740295bffa81215e5017b046978549867b
bbridgetty <bridgett2026@student.cis.edu.hk>
2023-09-12 14:17:26 +0800

    Quiz 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        //method 1
+        int result = add(2, 4);
+        System.out.println(result);
+        String greeting = frenchGreeting("Mr Ewing!");
+        System.out.println(greeting);
+        result = add(2, 4, 6, 8);
+        System.out.println(result);
+        String toBePrinted = toBePrinted("The End!");
+        System.out.println(toBePrinted);
+    }
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+    public static String frenchGreeting(String name) {return "Bonjour, " + name + "!";}
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {return numOne + numTwo + numThree + numFour;}
+    public static String toBePrinted(String printMe) {return printMe;}
 }
